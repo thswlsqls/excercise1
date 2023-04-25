746c0563cb540472afa00734810ea5e23e20e426
eunbinSon <thswlsqls@naver.com>
2023-04-25 14:50:31 +0900

    boj.kr/1697 s1

---
+++ boj.kr/BFS/1697.py
@@ -0,0 +1,18 @@
+from collections import deque
+N, K = map(int, input().split())
+chk = [0] * 100001
+
+def bfs():
+    dq = deque()
+    dq.append(N)
+    while dq:
+        cur = dq.popleft()
+        if cur == K:
+            print(chk[cur])
+            break
+        for nxt in (cur+1, cur-1, cur*2):
+            if 0 <= nxt <= 100000 and not chk[nxt]:
+               chk[nxt] = chk[cur] + 1
+               dq.append(nxt)
+
+bfs()
