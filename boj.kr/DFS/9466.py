1b5661d4b0b486e078b7c6a572f8ae22e99660b0
eunbinSon <thswlsqls@naver.com>
2023-05-03 16:50:37 +0900

    boj.kr/9466 g3

---
+++ boj.kr/DFS/9466.py
@@ -0,0 +1,31 @@
+import sys
+sys.setrecursionlimit(10**9)
+
+T = int(input())
+
+def dfs(node):
+    global team_members
+    cycle.append(node)
+    visited[node] = 1
+    nxt = choose[node]
+
+    if visited[nxt] == 1:
+        if nxt in cycle:
+            team_members += cycle[cycle.index(nxt):]
+    else:
+        dfs(nxt)
+
+
+for i in range(T):
+    N = int(input())
+    choose = [0] + list(map(int, input().split()))
+    team_members = []
+    visited = [0]*(N+1)
+
+    for n in range(1, N+1):
+        if visited[n] == 0:
+            cycle = []
+            dfs(n)
+
+    print(N - len(team_members))
+
