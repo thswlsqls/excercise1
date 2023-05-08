7c437a71fe65139d23aaf0a94a970c505a09332b
eunbinSon <thswlsqls@naver.com>
2023-05-08 16:59:46 +0900

    boj.kr/1946 s1

---
+++ boj.kr/greedy/1946.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+
+for _ in range(T):
+    N = int(input())
+    nums = [list(map(int, input().split())) for _ in range(N)]
+    nums = sorted(nums, key = lambda x: x[0])
+    compare = nums[0][1]
+    cnt = 1
+    print(nums)
+    for i in range(1, N):
+        if nums[i][1] < compare:
+            compare = nums[i][1]
+            cnt += 1
+
+    print(cnt)
