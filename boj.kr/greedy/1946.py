0298172c4462b601958a01a002cca7f232978ccb
eunbinSon <thswlsqls@naver.com>
2023-05-08 17:00:34 +0900

    boj.kr/1946 s1

--- boj.kr/greedy/1946.py
+++ boj.kr/greedy/1946.py
@@ -9,7 +9,6 @@ for _ in range(T):
     nums = sorted(nums, key = lambda x: x[0])
     compare = nums[0][1]
     cnt = 1
-    print(nums)
     for i in range(1, N):
         if nums[i][1] < compare:
             compare = nums[i][1]
