76d09747432495b9de3c482e49f34be1c35f4632
thswlsqls <thswlsqls@naver.com>
2023-05-27 19:25:52 +0900

    boj.kr/2470 g5

--- boj.kr/BinarySearch/2470.py
+++ boj.kr/BinarySearch/2470.py
@@ -9,18 +9,17 @@ tot = nums[p1] + nums[p2]
 min_tot = abs(tot)
 
 while p1 < p2:
-    if min_tot > abs(nums[p1] + nums[p2]):
+    tot_abs = abs(nums[p1] + nums[p2])
+    if min_tot > tot_abs:
         ans[0] = nums[p1]
         ans[1] = nums[p2]
-    min_tot = min(min_tot, abs(nums[p1] + nums[p2]))
+    min_tot = min(min_tot, tot_abs)
     tot = nums[p1] + nums[p2]
     if tot < 0:
         p1 += 1
     elif tot > 0:
         p2 -= 1
     else:
-        ans[0] = nums[p1]
-        ans[1] = nums[p2]
         break
 
 ans.sort()
