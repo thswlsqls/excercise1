9c2a7a9bb4839e7868c6581856583519846aa3b1
thswlsqls <thswlsqls@naver.com>
2023-06-04 15:12:48 +0900

    boj.kr/1149 s1

---
+++ boj.kr/dynamic/1149.py
@@ -0,0 +1,9 @@
+N = int(input())
+dp = [list(map(int, input().split())) for _ in range(N)]
+
+for i in range(1, len(dp)):
+    dp[i][0] += min(dp[i-1][1], dp[i-1][2])
+    dp[i][1] += min(dp[i-1][0], dp[i-1][2])
+    dp[i][2] += min(dp[i-1][0], dp[i-1][1])
+
+print(min(dp[N-1][0], dp[N-1][1], dp[N-1][2]))
