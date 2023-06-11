cb7f9e449b9aab918e8d15fe34cc9eb3f114eb15
thswlsqls <thswlsqls@naver.com>
2023-06-11 16:54:20 +0900

    boj.kr/9465 s1

---
+++ boj.kr/dynamic/9465.py
@@ -0,0 +1,13 @@
+T = int(input())
+
+for _ in range(T):
+    N = int(input())
+    dp = [list(map(int, input().split())) for _ in range(2)]
+    for i in range(1, N):
+        if i == 1:
+            dp[0][1] += dp[1][0]
+            dp[1][1] += dp[0][0]
+        else:
+            dp[0][i] += max(dp[1][i-1], dp[1][i-2])
+            dp[1][i] += max(dp[0][i-1], dp[0][i-2])
+    print(max(dp[0][N-1], dp[1][N-1]))
