308a92c2c22045a06076a1a3fa9d0a6b481c2756
thswlsqls <thswlsqls@naver.com>
2023-06-13 22:50:53 +0900

    boj.kt/1946 s1 review

--- boj.kr/greedy/1946.py
+++ boj.kr/greedy/1946.py
@@ -1,17 +1,15 @@
 import sys
 input = sys.stdin.readline
-
 T = int(input())
 
 for _ in range(T):
     N = int(input())
-    nums = [list(map(int, input().split())) for _ in range(N)]
-    nums = sorted(nums, key = lambda x: x[0])
-    compare = nums[0][1]
+    ranks = [list(map(int, input().split())) for _ in range(N)]
+    ranks = sorted(ranks, key = lambda x : x[0])
+    cur = ranks[0][1]
     cnt = 1
     for i in range(1, N):
-        if nums[i][1] < compare:
-            compare = nums[i][1]
+        if ranks[i][1] < cur:
+            cur = ranks[i][1]
             cnt += 1
-
     print(cnt)
