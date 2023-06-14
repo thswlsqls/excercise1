8423ae3e98eb3f75c0470ee501fd8bace12a4399
soneunbin <thswlsqls@naver.com>
2023-06-14 15:46:02 +0900

    boj.kr/2638 g3

--- boj.kr/BFS/2638.py
+++ boj.kr/BFS/2638.py
@@ -1,37 +1,39 @@
 N, M = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(N)]
 
+dy = [1, -1, 0, 0]
+dx = [0, 0, 1, -1]
+
 from collections import deque
 
-dy = (1, -1, 0, 0)
-dx = (0, 0, 1, -1)
-
-def bfs(y, x):
+def bfs():
+    chk = [[0 for _ in range(M)] for _ in range(N)]
     dq = deque()
-    dq.append((y, x))
-
+    dq.append((0, 0))
+    chk[0][0] = 1
     while dq:
-        (y, x) = dq.popleft()
+        (cy, cx) = dq.popleft()
         for i in range(4):
-            ny = y + dy[i]
-            nx = x + dx[i]
-            if 0<=ny<N and 0<=nx<M and ((graph[ny][nx] == 0 and visited[ny][nx] == 0) or (graph[ny][nx] == 1)):
-                visited[ny][nx] += 1
-                if graph[ny][nx] == 1 and visited[ny][nx] >= 2:
-                    graph[ny][nx] = 0
-                if graph[ny][nx] == 0 and visited[ny][nx] == 1:
+            ny = cy + dy[i]
+            nx = cx + dx[i]
+            if 0<=ny<N and 0<=nx<M and ((graph[ny][nx] == 0 and chk[ny][nx] == 0) or (graph[ny][nx] == 1 and not chk[ny][nx] > 2)):
+                chk[ny][nx] += 1
+                if graph[ny][nx] == 0:
                     dq.append((ny, nx))
+                if graph[ny][nx] == 1 and chk[ny][nx] == 2:
+                    graph[ny][nx] = 0
 
-cnt = 0
-while True:
-    visited = [[0] * M for _ in range(N)]
-    bfs(0, 0)
-    visited[0][0] = 1
-    cnt += 1
+def is_no_cheese_left():
     tot = 0
     for _ in graph:
         tot += sum(_)
-    if tot == 0:
-        break
+    return tot == 0
 
-print(cnt)
+hours = 0
+while True:
+    if is_no_cheese_left():
+        break
+    bfs()
+    hours += 1
+
+print(hours)
