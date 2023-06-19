b230fc6f58b7a7fa16dddc53d0ed84d516bc2fd2
soneunbin <thswlsqls@naver.com>
2023-06-19 15:46:25 +0900

    boj.kr/9466 g3 reveiew

--- boj.kr/DFS/9466.py
+++ boj.kr/DFS/9466.py
@@ -1,29 +1,27 @@
 import sys
-sys.setrecursionlimit(10**9)
+sys.setrecursionlimit(10 ** 9)
 
 T = int(input())
 
-def dfs(node):
-    global team_members
-    cycle.append(node)
-    visited[node] = 1
-    nxt = choose[node]
-
-    if visited[nxt] == 1:
+def dfs(cur):
+    global team
+    cycle.append(cur)
+    chk[cur] += 1
+    nxt = choose[cur]
+    if chk[nxt] == 1:
         if nxt in cycle:
-            team_members += cycle[cycle.index(nxt):]
+            team += cycle[cycle.index(nxt):]
     else:
         dfs(nxt)
 
-for i in range(T):
+for _ in range(T):
     N = int(input())
     choose = [0] + list(map(int, input().split()))
-    team_members = []
-    visited = [0]*(N+1)
-
-    for n in range(1, N+1):
-        if visited[n] == 0:
+    chk = [0] * (N+1)
+    team = []
+    for node in range(1, N+1):
+        if chk[node] == 0:
             cycle = []
-            dfs(n)
+            dfs(node)
 
-    print(N - len(team_members))
+    print(N-len(team))
