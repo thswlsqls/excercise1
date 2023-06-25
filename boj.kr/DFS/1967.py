a1f009c0674671282c0bf3d75c5aba40b397e786
thswlsqls <thswlsqls@naver.com>
2023-06-25 20:53:10 +0900

    boj.kr/1967 g4 review

--- boj.kr/DFS/1967.py
+++ boj.kr/DFS/1967.py
@@ -4,25 +4,24 @@ input = sys.stdin.readline
 
 N = int(input())
 graph = [[] for _ in range(N+1)]
-visited = [-1] * (N+1)
 
-for i in range(N-1):
-    (s, e, d) = map(int, input().split())
+for _ in range(N-1):
+    (s, e, d) = list(map(int, input().split()))
     graph[s].append([e, d])
     graph[e].append([s, d])
 
-def dfs(node, d):
-    for (ne, nd) in graph[node]:
-        if visited[ne] == -1:
-            visited[ne] = d+nd
-            dfs(ne, d+nd)
+def dfs(cur, d):
+    for (nxt, nd) in graph[cur]:
+        if visited[nxt] == -1:
+            visited[nxt] = d + nd
+            dfs(nxt, d + nd)
 
+visited = [-1] * (N+1)
 visited[1] = 0
 dfs(1, 0)
-
 start = visited.index(max(visited))
+
 visited = [-1] * (N+1)
 visited[start] = 0
 dfs(start, 0)
-
 print(max(visited))
