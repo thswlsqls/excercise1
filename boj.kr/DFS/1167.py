3037d73610ae467a26cb80306ad26acee95f9453
thswlsqls <thswlsqls@naver.com>
2023-06-25 18:15:17 +0900

    boj.rk/1167 g2 review

--- boj.kr/DFS/1167.py
+++ boj.kr/DFS/1167.py
@@ -1,18 +1,16 @@
 import sys
+sys.setrecursionlimit(10**9)
 input = sys.stdin.readline
-sys.setrecursionlimit(10**6)
 
 N = int(input())
 graph = [[] for _ in range(N+1)]
-visited = [-1] * (N+1)
-
 for _ in range(N):
-    nums = list(map(int, input().split()))
+    I = list(map(int, input().split()))
     s, e = 1, 3
-    while e < len(nums):
-        graph[nums[0]].append(nums[s:e])
-        s +=2
-        e +=2
+    while e < len(I):
+        graph[I[0]].append(I[s:e])
+        s += 2
+        e += 2
 
 def dfs(node, d):
     for (ne, nd) in graph[node]:
@@ -20,12 +18,12 @@ def dfs(node, d):
             visited[ne] = d + nd
             dfs(ne, d + nd)
 
+visited = [-1] * (N+1)
 visited[1] = 0
 dfs(1, 0)
-
 start = visited.index(max(visited))
+
 visited = [-1] * (N+1)
 visited[start] = 0
 dfs(start, 0)
-
 print(max(visited))
