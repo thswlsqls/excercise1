85db86cd62a6f298ebef86e6e3f1cf10d0e060e2
thswlsqls <thswlsqls@naver.com>
2023-07-01 12:35:47 +0900

    boj.kr/1520 g3 review

--- boj.kr/DFS/1520.py
+++ boj.kr/DFS/1520.py
@@ -1,27 +1,23 @@
 M, N = map(int, input().split())
-board = [list(map(int, input().split())) for _ in range(M)]
+graph = [list(map(int, input().split())) for _ in range(M)]
+dp = [[-1]*N for _ in range(M)]
 
 import sys
 sys.setrecursionlimit(10**9)
 
 dy = (0, 0, 1, -1)
 dx = (1, -1, 0, 0)
-
 def dfs(y, x):
     if dp[y][x] != -1:
         return dp[y][x]
     if y == M-1 and x == N-1:
         return 1
-
     dp[y][x] = 0
     for i in range(4):
         ny = y + dy[i]
         nx = x + dx[i]
-        if 0<=ny<M and 0<=nx<N:
-            if board[y][x] > board[ny][nx]:
-                dp[y][x] += dfs(ny, nx)
-
+        if 0<=ny<M and 0<=nx<N and graph[y][x] > graph[ny][nx]:
+            dp[y][x] += dfs(ny, nx)
     return dp[y][x]
 
-dp = [[-1]*N for _ in range(M)]
-print(dfs(0,0))
+print(dfs(0, 0))
