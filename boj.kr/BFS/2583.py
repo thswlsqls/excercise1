628f6b4b382792d89782da2b5c265ebcf67c35c4
soneunbin <thswlsqls@naver.com>
2023-07-03 16:47:29 +0900

    boj.kr/2583 s1 review

--- boj.kr/BFS/2583.py
+++ boj.kr/BFS/2583.py
@@ -1,41 +1,38 @@
 M, N, K = map(int, input().split())
-coord = [list(map(int, input().split())) for _ in range(K)]
-board = [[0]*N for _ in range(M)]
-
-for i in range(K):
-    for yi in range(coord[i][1], coord[i][3]):
-        for xi in range(coord[i][0], coord[i][2]):
-            board[yi][xi] = 1
-
-from collections import deque
+graph = [[0]*N for _ in range(M)]
+for _ in range(K):
+    (x1, y1, x2, y2) = map(int, input().split())
+    for cy in range(y1, y2):
+        for cx in range(x1, x2):
+            graph[cy][cx] += 1
 
 dy = (0, 0, -1, 1)
 dx = (-1, 1, 0, 0)
-
-chk = [[0]*N for _ in range(M)]
+from collections import deque
+visited = [[0]*N for _ in range(M)]
 def bfs(y, x):
+    cnt = 1
     dq = deque()
     dq.append((y, x))
-    size = 1
     while dq:
-        (y, x) = dq.popleft()
+        (cy, cx) = dq.popleft()
         for i in range(4):
-            ny = y + dy[i]
-            nx = x + dx[i]
-            if 0<=ny<M and 0<=nx<N and board[ny][nx] == 0 and not chk[ny][nx]:
-                chk[ny][nx] = chk[ny][nx] + 1
+            ny = cy + dy[i]
+            nx = cx + dx[i]
+            if 0<=ny<M and 0<=nx<N and graph[ny][nx] == 0 and visited[ny][nx] == 0:
+                visited[ny][nx] += 1
                 dq.append((ny, nx))
-                size += 1
-    return size
+                cnt += 1
+    return cnt
 
-cnt = 0
-size_list = []
-for y in range(M):
-    for x in range(N):
-        if board[y][x] == 0 and not chk[y][x]:
-            chk[y][x] = chk[y][x] + 1
-            size_list.append(bfs(y, x))
-            cnt += 1
+cnt_list = []
+b_cnt = 0
+for fy in range(M):
+    for fx in range(N):
+        if graph[fy][fx] == 0 and visited[fy][fx] == 0:
+            visited[fy][fx] += 1
+            cnt_list.append(bfs(fy, fx))
+            b_cnt += 1
 
-print(cnt)
-print(*sorted(size_list))
+print(b_cnt)
+print(*sorted(cnt_list))
