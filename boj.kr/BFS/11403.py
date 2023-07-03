3db18253b4b80a3347748bdf38b337e651d5454c
thswlsqls <thswlsqls@naver.com>
2023-07-03 17:26:26 +0900

    boj.kr/11403 s1 review

--- boj.kr/BFS/11403.py
+++ boj.kr/BFS/11403.py
@@ -1,11 +1,11 @@
 N = int(input())
-adj = [list(map(int, input().split())) for _ in range(N)]
+graph = [list(map(int, input().split())) for _ in range(N)]
 
 for k in range(N):
-    for y in range(N):
-        for x in range(N):
-            if adj[y][x] == 1 or (adj[y][k] == 1 and adj[k][x] == 1):
-                adj[y][x] = 1
+    for s in range(N):
+        for e in range(N):
+            if graph[s][k] == 1 and graph[k][e] == 1:
+                graph[s][e] = 1
 
-for _ in adj:
-    print(*_)
+for row in graph:
+    print(*row)
