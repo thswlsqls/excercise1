239aeb9a458bce8604a1605eed85f8309c8a8f08
thswlsqls <thswlsqls@naver.com>
2023-07-03 17:10:07 +0900

    boj.kr/1931 s1 review

--- boj.kr/greedy/1931.py
+++ boj.kr/greedy/1931.py
@@ -2,6 +2,8 @@ N = int(input())
 times = [list(map(int, input().split())) for _ in range(N)]
 times = sorted(times, key=lambda x : (x[1], x[0]))
 
+print(times)
+
 last_e = 0
 cnt = 0
 for [s, e] in times:
