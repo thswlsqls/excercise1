7dfecf26fd0a258f5a7d52fbb3dbbdaaeb88c947
thswlsqls <thswlsqls@naver.com>
2023-07-04 10:35:17 +0900

    boj.kr/7562 s1 review

--- boj.kr/BFS/7562.py
+++ boj.kr/BFS/7562.py
@@ -5,7 +5,6 @@ dx = (-1, 1, -2, 2, -2, 2, -1, 1)
 
 from collections import deque
 def bfs(sx, sy, ex, ey, N):
-    visited = [[0]*N for _ in range(N)]
     dq = deque()
     dq.append((sy, sx, 0))
     while dq:
@@ -15,8 +14,8 @@ def bfs(sx, sy, ex, ey, N):
         for i in range(8):
             ny = cy + dy[i]
             nx = cx + dx[i]
-            if 0<=ny<N and 0<=nx<N and visited[ny][nx] == 0:
-                visited[ny][nx] += 1
+            if 0<=ny<N and 0<=nx<N and graph[ny][nx] == 0:
+                graph[ny][nx] += 1
                 dq.append((ny, nx, d+1))
 
 for _ in range(T):
