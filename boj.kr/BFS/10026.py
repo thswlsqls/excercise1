3c866b3702cb3d9a67c2b7d0e97f16c0eae0e8a0
thswlsqls <thswlsqls@naver.com>
2023-07-04 15:03:21 +0900

    boj.kr/10026 g5 review

--- boj.kr/BFS/10026.py
+++ boj.kr/BFS/10026.py
@@ -1,54 +1,44 @@
-# 1. board를 순회하면서 chk한 칸이 아니면 bfs방식으로 모두 탐색, 영역개수+1
-# 2. 비색약자인 경우, 색약자인 경우 각각 탐색
-# 3. 각각의 경우의 영역 총 개수를 출력
-
-from collections import deque
-
 N = int(input())
-board = [list(input()) for _ in range(N)]
+graph = [input() for _ in range(N)]
 
-dx = (1, -1, 0, 0)
-dy = (0, 0, 1, -1)
-
-def bfs(rgb, x, y):
+dy = (-1, 1, 0, 0)
+dx = (0, 0, -1, 1)
+from collections import deque
+def bfs(y, x, rgb):
     dq = deque()
-    dq.append((x, y))
-
+    dq.append((y, x))
     while dq:
-        (x, y) = dq.popleft()
+        (cy, cx) = dq.popleft()
         for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
+            ny = cy + dy[i]
+            nx = cx + dx[i]
             if rgb == 'RG':
-                if 0 <= nx < N and 0 <= ny < N and (board[nx][ny] == 'R' or board[nx][ny] == 'G') and not chk[nx][ny]:
-                    dq.append((nx, ny))
-                    chk[nx][ny] = True
+                if 0<=ny<N and 0<=nx<N and visited[ny][nx] == 0 and (graph[ny][nx] == 'R' or graph[ny][nx] == 'G'):
+                        visited[ny][nx] += 1
+                        dq.append((ny, nx))
             else:
-                if 0<=nx<N and 0<=ny<N and board[nx][ny] == rgb and not chk[nx][ny]:
-                    dq.append((nx, ny))
-                    chk[nx][ny] = True
+                if 0<=ny<N and 0<=nx<N and visited[ny][nx] == 0 and graph[ny][nx] == rgb:
+                        visited[ny][nx] += 1
+                        dq.append((ny, nx))
 
-(R_cnt, G_cnt, B_cnt) = (0, 0, 0)
-chk = [[False]*N for _ in range(N)]
-for x in range(N):
-    for y in range(N):
-        if board[x][y] == 'R' and not chk[x][y]:
-            bfs('R', x, y)
-            R_cnt+=1
-        elif board[x][y] == 'G' and not chk[x][y]:
-            bfs('G', x, y)
-            G_cnt+=1
-        elif board[x][y] == 'B' and not chk[x][y]:
-            bfs('B', x, y)
-            B_cnt+=1
-
+visited = [[0]*N for _ in range(N)]
+RGB_cnt = 0
+for y1 in range(N):
+    for x1 in range(N):
+        if visited[y1][x1] == 0:
+            bfs(y1, x1, graph[y1][x1])
+            RGB_cnt += 1
+visited = [[0]*N for _ in range(N)]
 RG_cnt = 0
-chk = [[False]*N for _ in range(N)]
-for x in range(N):
-    for y in range(N):
-        if (board[x][y] == 'R' or board[x][y] == 'G') and not chk[x][y]:
-            bfs('RG', x, y)
-            RG_cnt+=1
+for y2 in range(N):
+    for x2 in range(N):
+        if visited[y2][x2] == 0:
+            if graph[y2][x2] == 'R' or graph[y2][x2] == 'G':
+                bfs(y2, x2, 'RG')
+                RG_cnt += 1
+            else:
+                bfs(y2, x2, 'B')
+                RG_cnt += 1
 
-print(R_cnt+G_cnt+B_cnt)
-print(RG_cnt+B_cnt)
+print(RGB_cnt, end=' ')
+print(RG_cnt)
