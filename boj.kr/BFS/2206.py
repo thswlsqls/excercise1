5c5cd52f4d0b89f3ea6b51efd02dc34004abb180
thswlsqls <thswlsqls@naver.com>
2023-07-04 09:42:48 +0900

    boj.kr/2206 g3 review

--- boj.kr/BFS/2206.py
+++ boj.kr/BFS/2206.py
@@ -1,35 +1,27 @@
-from collections import deque
-
 N, M = map(int, input().split())
-board = [list(input()) for _ in range(N)]
+graph = [input() for _ in range(N)]
+visited = [[[0]*2 for _ in range(M)] for _ in range(N)]
 
-dy = (1, -1, 0, 0)
-dx = (0, 0, 1, -1)
-
+dy = (0, 0, -1, 1)
+dx = (-1, 1, 0, 0)
+from collections import deque
 def bfs():
-    chk = [[[0]*2 for _ in range(M)] for _ in range(N)]
-
     dq = deque()
-    dq.append((0, 0, 1))
-    chk[0][0][1] = 1
-
+    dq.append((0, 0, 1, 1))
+    visited[0][0][1] += 1
     while dq:
-        (y, x, break_left) = dq.popleft()
-
-        if x == M - 1 and y == N - 1:
-            return chk[y][x][break_left]
-
+        (cy, cx, left, d) = dq.popleft()
+        if cy == N-1 and cx == M-1:
+            return d
         for i in range(4):
-            ny = y + dy[i]
-            nx = x + dx[i]
-            if 0 <= nx < M and 0 <= ny < N and board[ny][nx] == '0' and not chk[ny][nx][break_left]:
-                chk[ny][nx][break_left] = chk[y][x][break_left] + 1
-                dq.append((ny, nx, break_left))
-            if 0 <= nx < M and 0 <= ny < N and board[ny][nx] == '1' and break_left == 1:
-                chk[ny][nx][break_left] = chk[y][x][break_left] + 1
-                chk[ny][nx][break_left -1] = chk[y][x][break_left] + 1
-                dq.append((ny, nx, break_left-1))
-
+            ny = cy + dy[i]
+            nx = cx + dx[i]
+            if 0<=ny<N and 0<=nx<M and visited[ny][nx][left] == 0:
+                if graph[ny][nx] == '1' and left == 1:
+                    dq.append((ny, nx, 0, d+1))
+                    visited[ny][nx][0] += 1
+                elif graph[ny][nx] == '0':
+                    dq.append((ny, nx, left, d+1))
+                    visited[ny][nx][left] += 1
     return -1
-
 print(bfs())
