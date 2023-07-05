db98d98a8d900353262f3f1dc91466650a56b050
thswlsqls <thswlsqls@naver.com>
2023-07-05 09:14:05 +0900

    boj.kr/14502 g4 review

--- boj.kr/BFS/14502.py
+++ boj.kr/BFS/14502.py
@@ -1,55 +1,44 @@
-# 1. 벽을 세울 수 있는 3개 좌표 추출하는 모든 경우에 대해 아래 반복
-# 2. BFS함수를 통해 벽을 세운 후 전염결과 구하기
-# 3. 전염후 안전지대 총 개수 계산
-
-from itertools import combinations
-import copy
-from collections import deque
-
 N, M = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(N)]
 
-dx = (1, -1, 0, 0)
-dy = (0, 0, 1, -1)
+space = []
+infected = []
+for y in range(N):
+    for x in range(M):
+        if board[y][x] == 0:
+            space.append((y, x))
+        if board[y][x] == 2:
+            infected.append((y, x))
 
-def bfs(temp):
-    chk = [[False] * M for _ in range(N)]
+dy = (0, 0, -1, 1)
+dx = (-1, 1, 0, 0)
+from collections import deque
+def bfs(y, x, temp):
     dq = deque()
-
-    for x in range(N):
-        for y in range(M):
-            if temp[x][y] == 2:
-                dq.append((x, y))
-
+    dq.append((y, x))
     while dq:
-        (x, y) = dq.popleft()
+        (cy, cx) = dq.popleft()
         for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0<=nx<N and 0<=ny<M and temp[nx][ny] == 0 and not chk[nx][ny]:
-                chk[nx][ny] = True
-                temp[nx][ny] = 2
-                dq.append((nx, ny))
+            ny = cy + dy[i]
+            nx = cx + dx[i]
+            if 0<=ny<N and 0<=nx<M and (temp[ny][nx] == 0 or temp[ny][nx] == 2) and visited[ny][nx] == 0:
+                visited[ny][nx] += 1
+                temp[ny][nx] = 2
+                dq.append((ny, nx))
 
-candidates = []
-for x in range(N):
-    for y in range(M):
-        if board[x][y] == 0:
-            candidates.append([x, y])
-
-max_cnt_0 = -1e9
-for combi in combinations(candidates, 3):
+import copy
+from itertools import combinations
+safe_list = []
+for combi in combinations(space, 3):
+    visited = [[0] * M for _ in range(N)]
     temp = copy.deepcopy(board)
-    for [x, y] in combi:
-        temp[x][y] = 1
-    bfs(temp)
-    cnt_0 = 0
+    for (y1, x1) in combi:
+        temp[y1][x1] = 1
+    for (y2, x2) in infected:
+        visited[y2][x2] += 1
+        bfs(y2, x2, temp)
+    safe = 0
     for row in temp:
-        cnt_0 += row.count(0)
-    max_cnt_0 = cnt_0 if max_cnt_0 < cnt_0 else max_cnt_0
-
-print(max_cnt_0)
-
-
-
-
+        safe += row.count(0)
+    safe_list.append(safe)
+print(max(safe_list))
