99796862fee7d18ed14365ce086db50616ea572b
thswlsqls <thswlsqls@naver.com>
2023-07-05 09:43:23 +0900

    boj.kr/1697 s1 review

--- boj.kr/BFS/1697.py
+++ boj.kr/BFS/1697.py
@@ -1,17 +1,16 @@
-from collections import deque
 N, K = map(int, input().split())
-chk = [0] * 100001
-
+from collections import deque
 def bfs(N):
+    visited = [0] * 100001
+    visited[N] += 1
     dq = deque()
-    dq.append(N)
+    dq.append((N, 0))
     while dq:
-        cur = dq.popleft()
+        (cur, cnt) = dq.popleft()
         if cur == K:
-            return chk[cur]
-        for nxt in (cur+1, cur-1, cur*2):
-            if 0 <= nxt <= 100000 and not chk[nxt]:
-               chk[nxt] = chk[cur] + 1
-               dq.append(nxt)
-
+            return cnt
+        for nxt in (cur*2, cur+1, cur-1):
+            if 0<=nxt<100001 and visited[nxt] == 0:
+                visited[nxt] += 1
+                dq.append((nxt, cnt+1))
 print(bfs(N))
