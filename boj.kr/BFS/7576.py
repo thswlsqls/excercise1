62ff90e03e5d7401bb484b6864444ccda7060482
thswlsqls <thswlsqls@naver.com>
2023-07-06 09:12:01 +0900

    boj.kr/7576 g5 review

--- boj.kr/BFS/7576.py
+++ boj.kr/BFS/7576.py
@@ -1,37 +1,30 @@
-# 1. deque에 익은 토마토 좌표를 모두 저장
-# 2. dfs 방식으로 deque를 검사하면서 상화좌우 안익은 토마토는 현재토마토 좌표의 값의 + 1의 값을 갖도록 저장
-# 3. 익은 토마토로 변경된 토마토의 좌표를 deque에 저장
-# 4. 전제 좌표 값을 순회하면서 0이 있으면 -1을 출력, 0이 없으면 좌표 값 중에서 최대-1을 출력
-
-from collections import deque
-
 M, N = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(N)]
 
-dx = (1, -1, 0, 0)
-dy = (0, 0, 1, -1)
-
+from collections import deque
 dq = deque()
-for x in range(N):
-    for y in range(M):
-        if board[x][y] == 1:
-            dq.append((x, y))
+for y in range(N):
+    for x in range(M):
+        if board[y][x] == 1:
+            dq.append((y, x))
+
+dy = (1, -1, 0, 0)
+dx = (0, 0, 1, -1)
 def bfs():
     while dq:
-        (x, y) = dq.popleft()
+        (cy, cx) = dq.popleft()
         for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0<=nx<N and 0<=ny<M and board[nx][ny] == 0:
-                    board[nx][ny] = board[x][y] + 1
-                    dq.append((nx, ny))
+            ny = cy + dy[i]
+            nx = cx + dx[i]
+            if 0<=ny<N and 0<=nx<M and board[ny][nx] == 0:
+                board[ny][nx] = board[cy][cx] + 1
+                dq.append((ny, nx))
     max_cnt = -1e9
-    for x in range(N):
-        for y in range(M):
-            if board[x][y] == 0:
-                return -1
-            else:
-                max_cnt = board[x][y] if max_cnt < board[x][y] else max_cnt
+    for row in board:
+        if row.count(0) > 0:
+            return -1
+        else:
+            max_cnt = max(max_cnt, max(row))
     return max_cnt-1
 
 print(bfs())
