90e4625b9ead6c0433acfea7c8db8414e7e59c9f
thswlsqls <thswlsqls@naver.com>
2023-07-06 10:12:52 +0900

    boj.kr/2178 s1 review

--- boj.kr/BFS/2178.py
+++ boj.kr/BFS/2178.py
@@ -1,27 +1,22 @@
-from collections import deque
-
 N, M = map(int, input().split())
-board = [list(input()) for _ in range(N)]
+board = [input().strip() for _ in range(N)]
+visited = [[0] * M for _ in range(N)]
 
-dx = (1, -1, 0, 0)
-dy = (0, 0, 1, -1)
-
+dy = (1, -1, 0, 0)
+dx = (0, 0, 1, -1)
+from collections import deque
 def bfs():
-    chk = [[False]*M for _ in range(N)]
-    que = deque()
-    que.append((0, 0, 1))
-    while que:
-        (x, y, d) = que.popleft()
-
-        if x == N-1 and y == M-1:
+    dq = deque()
+    dq.append((0, 0, 1))
+    while dq:
+        (cy, cx, d) = dq.popleft()
+        if cy == N - 1 and cx == M - 1:
             return d
-
-        nd = d + 1
         for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0<=nx<N and 0<=ny<M and board[nx][ny] == '1' and not chk[nx][ny]:
-                chk[nx][ny] = True
-                que.append((nx, ny, nd))
+            ny = cy + dy[i]
+            nx = cx + dx[i]
+            if 0 <= ny < N and 0 <= nx < M and board[ny][nx] == '1' and visited[ny][nx] == 0:
+                visited[ny][nx] += 1
+                dq.append((ny, nx, d + 1))
 
 print(bfs())
