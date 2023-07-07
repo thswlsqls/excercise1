12a514486a91f596d9cd004862a0756c8713bb81
thswlsqls <thswlsqls@naver.com>
2023-07-07 11:45:53 +0900

    boj.kr/15686 g5 review

--- boj.kr/bruteforce/15686.py
+++ boj.kr/bruteforce/15686.py
@@ -1,36 +1,24 @@
-# 1. 집의 좌표들과 치킨집 좌표들을 각각 리스트에 저장
-# 2. combinations를 사용해 치킨집 좌표 리스트에서 추출한 M개의 치킨집 좌표들을 순회하면서 아래(3,4,5)를 반복
-# 3. 집의 좌표 목록을 순회
-# 4. 집에서 치킨집까지 거리를 계산하고 최소 치킨거리를 저장
-# 5. 모든 집들의 최소 치킨거리를 더한 값을 최소 치킨거리 리스트에 저장
-# 6. 최소 치킨거리 리스트에서 최소값을 출력
-
-from itertools import combinations
-
 N, M = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(N)]
-
-homes = []
 chickens = []
+homes = []
+for y  in range(N):
+    for x in range(N):
+        if board[y][x] == 2:
+            chickens.append((y, x))
+        if board[y][x] == 1:
+            homes.append((y, x))
+def get_d(y1, x2, y2, x1):
+    return abs(y2-y1) + abs(x2-x1)
 
-for i in range(N):
-    for j in range(N):
-        if board[i][j] == 1:
-            homes.append([i,j])
-        elif board[i][j] == 2:
-            chickens.append([i,j])
-
-def distance(a, b):
-    return abs(a[0]-b[0]) + abs(a[1]-b[1])
-
+from itertools import combinations
 chicken_d_sum_list = []
-for M_chickens in combinations(chickens, M):
+for combi in combinations(chickens, M):
     chicken_d_sum = 0
-    for a in homes:
-        chicken_d = 1e9
-        for b in M_chickens:
-            chicken_d = distance(a, b) if chicken_d > distance(a, b) else chicken_d
-        chicken_d_sum += chicken_d
+    for (hy, hx) in homes:
+        min_chicken_d = 1e9
+        for (cy, cx) in combi:
+            min_chicken_d = min(min_chicken_d, get_d(hy, hx, cy, cx))
+        chicken_d_sum += min_chicken_d
     chicken_d_sum_list.append(chicken_d_sum)
-
-print(sorted(chicken_d_sum_list)[0])
+print(min(chicken_d_sum_list))
