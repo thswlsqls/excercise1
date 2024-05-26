d4a9b5984072327225a11516c1ad9e3a8821208b
thswlsqls <thswlsqls@naver.com>
2024-05-26 20:02:15 +0900

    commit changes

--- boj.kr/bruteforce/1018_1.py
+++ boj.kr/bruteforce/1018_1.py
@@ -13,16 +13,16 @@ for r in range(N):
                 if board[r][c] == 'B':
                     cntBoardW[r][c] = 1
             if c % 2 == 1:
-                if board[r][c] == 'B':
-                    cntBoardB[r][c] = 1
                 if board[r][c] == 'W':
                     cntBoardW[r][c] = 1
+                if board[r][c] == 'B':
+                    cntBoardB[r][c] = 1
         if r % 2 == 1:
             if c % 2 == 0:
-                if board[r][c] == 'B':
-                    cntBoardB[r][c] = 1
                 if board[r][c] == 'W':
                     cntBoardW[r][c] = 1
+                if board[r][c] == 'B':
+                    cntBoardB[r][c] = 1
             if c % 2 == 1:
                 if board[r][c] == 'W':
                     cntBoardB[r][c] = 1
@@ -35,10 +35,12 @@ cntW = 0
 
 for i in range(N-7):
     for j in range(M-7):
-        for k in range(8):
-            for l in range(8):
-                cntB += cntBoardB[i+k][j+l]
-                cntW += cntBoardW[i+k][j+l]
+        for r in range(8):
+            for c in range(8):
+                if cntBoardB[i+r][j+c] == 1:
+                    cntB += 1
+                if cntBoardW[i+r][j+c] == 1:
+                    cntW += 1
         cntList.append(cntB)
         cntList.append(cntW)
         cntB = 0
