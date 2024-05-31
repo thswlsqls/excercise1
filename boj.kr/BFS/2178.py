9e1de98b249a74436faf6e17776a1059e3e52345
thswlsqls <thswlsqls@naver.com>
2024-06-01 07:44:06 +0900

    commit changes

--- boj.kr/BFS/2178.py
+++ boj.kr/BFS/2178.py
@@ -2,21 +2,21 @@ N, M = map(int, input().split())
 board = [input().strip() for _ in range(N)]
 visited = [[0] * M for _ in range(N)]
 
-dy = (1, -1, 0, 0)
-dx = (0, 0, 1, -1)
+dr = (1, -1, 0, 0)
+dc = (0, 0, 1, -1)
 from collections import deque
 def bfs():
     dq = deque()
     dq.append((0, 0, 1))
     while dq:
-        (cy, cx, d) = dq.popleft()
-        if cy == N - 1 and cx == M - 1:
+        (cr, cc, d) = dq.popleft()
+        if cr == N-1 and cc == M-1:
             return d
         for i in range(4):
-            ny = cy + dy[i]
-            nx = cx + dx[i]
-            if 0 <= ny < N and 0 <= nx < M and board[ny][nx] == '1' and visited[ny][nx] == 0:
-                visited[ny][nx] += 1
-                dq.append((ny, nx, d + 1))
+            nr = cr + dr[i]
+            nc = cc + dc[i]
+            if 0<=nr<N and 0<=nc<M and board[nr][nc] == '1' and visited[nr][nc] == 0:
+                visited[nr][nc] = 1
+                dq.append((nr, nc, d+1))
 
 print(bfs())
